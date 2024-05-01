8e137ec15e12379ebfb31ec882578cba460fb2c6
hibana2077 <hibana2077@gmaill.com>
2024-05-01 16:12:12 +0800

    Update gen_data.py to load data for "Kāneiolouma Complex"

--- data/gen_data.py
+++ data/gen_data.py
@@ -2,10 +2,11 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-05-01 15:09:23
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-05-01 15:25:33
+LastEditTime: 2024-05-01 15:48:56
 FilePath: /core-LLM/data/gen_data.py
 Description: 
 '''
+import yaml
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from warnings import filterwarnings
@@ -14,5 +15,5 @@ from langchain_community.document_loaders import WikipediaLoader
 filterwarnings("ignore")
 
 # load data
-docs = WikipediaLoader(query="La Salute è in voi", load_max_docs=2).load()
+docs = WikipediaLoader(query="Kāneiolouma Complex", load_max_docs=1).load()
 print(docs[0].page_content)
