acc5ddb81493517f1059cb7f9a33150967ecd7dc
hibana2077 <hibana2077@gmaill.com>
2024-05-01 16:22:38 +0800

    Update config.yaml with job and huggingface settings

--- data/gen_data.py
+++ data/gen_data.py
@@ -2,11 +2,11 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-05-01 15:09:23
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-05-01 15:48:56
+LastEditTime: 2024-05-01 16:16:38
 FilePath: /core-LLM/data/gen_data.py
 Description: 
 '''
-import yaml
+from yaml import safe_load
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from warnings import filterwarnings
